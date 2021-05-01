599ae6ba2b3e5371ddfb7a2a40b419c659b9869c
name165 <1234@ruu.kr>
2021-05-01 15:46:42 +0900

    11

---
+++ 문제300/문제1-10.py
@@ -0,0 +1,21 @@
+#문제 1
+print("Hello World")
+#문제 2
+print("Mary's cosmetics")
+#문제 3
+print('''신씨가 소리쳤다. "도둑이야".''')
+#문제 4
+print('"C:\\Windows')
+#문제 5
+print("안녕하세요.\n,만나서\t\t반갑습니다.")
+#문제 6
+print("오늘은", "일요일")
+#문제 7
+print("naver", "kakao", "sk", "samsumg", sep=";")
+#문제 8
+print("naver", "kakao", "sk", "samsumg", sep="/")
+#문제 9
+print("first ",end=''); print("second")
+#문제 10
+print(5/3)
+
