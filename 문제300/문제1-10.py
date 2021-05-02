790aa0a0ef02c5e932bf231b11868fd6cc3e4a42
name165 <1234@ruu.kr>
2021-05-02 14:41:13 +0900

    1

--- 문제300/문제1-10.py
+++ 문제300/문제1-10.py
@@ -146,3 +146,69 @@ print(date.split("-"))
 #문제50
 data="039490    "
 print(data.rstrip())
+#문제51
+movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
+print(movie_rank)
+#문제52
+movie_rank.append("배트맨")
+print(movie_rank)
+#문제53
+movie_rank.insert(1,"슈퍼맨")
+print(movie_rank)
+#문제54
+movie_rank.remove("럭키")
+print(movie_rank)
+#문제55
+movie_rank.remove("스플릿")
+movie_rank.remove("배트맨")
+print(movie_rank)
+#문제56
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+langs = lang1 + lang2
+print(langs)
+#문제57
+nums = [1, 2, 3, 4, 5, 6, 7]
+print("max :", max(nums))
+print("min :", min(nums))
+#문제58
+nums = [1, 2, 3, 4 ,5]
+print(sum(nums))
+#문제59
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+#문제60
+nums = [1, 2, 3, 4, 5]
+print(sum(nums)/len(nums))
+#문제61
+price = ["20180728", 100, 130, 140, 150, 160, 170]
+print(price[1:])
+#문제62
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(nums[::2])
+#문제63
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(nums[1::2])
+#문제64
+nums = [1, 2, 3, 4, 5]
+print(nums[4::-1])
+#문제65
+interest = ['삼성전자', "LG전자", "Naver"]
+print(interest[0::2])
+#문제66
+interest = ['삼성전자', "LG전자", "Naver", "SK하이닉스", "미래에셋대우"]
+print(" ".join(interest))
+#문제67
+interest = ['삼성전자', "LG전자", "Naver", "SK하이닉스", "미래에셋대우"]
+print("/".join(interest))
+#문제68
+interest = ['삼성전자', "LG전자", "Naver", "SK하이닉스", "미래에셋대우"]
+print("\n".join(interest))
+#문제69
+string = "삼성전자/LG전자/Naver"
+interest = string.split("/")
+print(interest)
+#문제70
+data = [2, 4, 3, 1, 5, 10, 9]
+data.sort()
+print(data)
